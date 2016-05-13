1e8ac02177c7ac6fffe49099ac19a2982f85c25f
Dan Monroe <daniel.monroe@cardinalhealth.com>
2016-05-12 20:16:40 -0400

    Small change to get travis to build

--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -4,6 +4,6 @@ defmodule ContestDirectorApi.SessionController do
   def index(conn, _params) do
     # Return some static JSON for now
     conn
-    |> json(%{status: "Ok!!"})
+    |> json(%{status: "Ok!!!"})
   end
 end
